f36a27722fcc848c783cd0fc569c4938f2009f10
Mark Alston <malston@pivotal.io>
2017-04-03 20:01:52 -0500

    Configure integration tests

--- src/test/java/io/pivotal/hellotomcat/launch/MainTest.java
+++ src/test/java/io/pivotal/hellotomcat/launch/MainTest.java
@@ -3,15 +3,21 @@ package io.pivotal.hellotomcat.launch;
 import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.experimental.categories.Category;
 
-public class MainTest {
+public class MainTest extends Assert {
     private Main main = new Main();
 
+    @Rule
+    public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
     @Test
-    @Ignore
+    @Category(IntegrationTest.class)
     public void testRunStartsTomcat() throws Exception {
+        environmentVariables.set("SPRING_PROFILES_ACTIVE", "development,db");
         Tomcat tomcat = main.run("http://localhost:8888");
         Assert.assertNotNull(tomcat);
         StandardServer server = (StandardServer) tomcat.getServer();
